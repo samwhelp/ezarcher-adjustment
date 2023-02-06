a9a6a95d66801a6942318ab56e915bb46147b1aa
samwhelp <samwhelp@users.noreply.github.com>
2023-02-07 06:30:10 +0800

    fix

--- project/ezarcher-adjustment-system/ezarcher-adjustment-packaging/pack/core/tool/ezarcher-adjustment-modeling-thunar/asset/overlay/etc/skel/.config/Thunar/accels.scm
+++ project/ezarcher-adjustment-system/ezarcher-adjustment-packaging/pack/core/tool/ezarcher-adjustment-modeling-thunar/asset/overlay/etc/skel/.config/Thunar/accels.scm
@@ -1,3 +1,2 @@
 (gtk_accel_path "<Actions>/ThunarActions/uca-action-Action_Terminal-010" "F4")
-(gtk_accel_path "<Actions>/ThunarActions/uca-action-Action_File_Compare-010" "F3")
 (gtk_accel_path "<Actions>/ThunarActions/uca-action-Action_Text_Editor-010" "F2")
