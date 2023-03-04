ce31493edbfe8d46cdca419c112601a9057c3467
samwhelp <samwhelp@users.noreply.github.com>
2023-03-04 19:39:04 +0800

    fix

--- prototype/wayland/newm/part/newm-config-main/config/newm/config.py
+++ prototype/wayland/newm/part/newm-config-main/config/newm/config.py
@@ -23,7 +23,8 @@ background = {
 
 outputs = [
     { 'name': 'eDP-1' },
-    { 'name': 'virt-1', 'pos_x': -1280, 'pos_y': 0, 'width': 1280, 'height': 720 }
+   # { 'name': 'virt-1', 'pos_x': -1280, 'pos_y': 0, 'width': 1280, 'height': 720 }
+    { 'name': 'virt-1', 'pos_x': -1280, 'pos_y': 0, 'width': 1280, 'height': 1280 }
 ]
 
 wob_runner = WobRunner("wob -a bottom -M 100")
@@ -36,35 +37,54 @@ def synchronous_update() -> None:
 pactl = PaCtl(0, wob_runner)
 
 def key_bindings(layout: Layout) -> list[tuple[str, Callable[[], Any]]]:
-    return [
-        ("L-h", lambda: layout.move(-1, 0)),
-        ("L-j", lambda: layout.move(0, 1)),
-        ("L-k", lambda: layout.move(0, -1)),
-        ("L-l", lambda: layout.move(1, 0)),
-        ("L-u", lambda: layout.basic_scale(1)),
-        ("L-n", lambda: layout.basic_scale(-1)),
-        ("L-t", lambda: layout.move_in_stack(1)),
 
-        ("L-H", lambda: layout.move_focused_view(-1, 0)),
-        ("L-J", lambda: layout.move_focused_view(0, 1)),
-        ("L-K", lambda: layout.move_focused_view(0, -1)),
-        ("L-L", lambda: layout.move_focused_view(1, 0)),
+    return [    
 
-        ("L-C-h", lambda: layout.resize_focused_view(-1, 0)),
-        ("L-C-j", lambda: layout.resize_focused_view(0, 1)),
-        ("L-C-k", lambda: layout.resize_focused_view(0, -1)),
-        ("L-C-l", lambda: layout.resize_focused_view(1, 0)),
+        #("L-p", lambda: layout.ensure_locked(dim=True)),
+        #("L-P", lambda: layout.terminate()),
 
-        ("L-Return", lambda: os.system("alacritty &")),
-        ("L-q", lambda: layout.close_focused_view()),
+        ("A-S-c", lambda: layout.update_config()),
+        ("A-S-x", lambda: os.system("wlogout &")),
 
-        ("L-p", lambda: layout.ensure_locked(dim=True)),
-        ("L-P", lambda: layout.terminate()),
-        ("L-C", lambda: layout.update_config()),
+
+
+
+        #("L-", lambda: layout.toggle_overview()),
+        ("L-grave", lambda: layout.toggle_overview()),
 
         ("L-f", lambda: layout.toggle_fullscreen()),
+        ("L-q", lambda: layout.close_focused_view()),
 
-        ("L-", lambda: layout.toggle_overview()),
+
+        ("L-u", lambda: layout.basic_scale(-1)),
+        ("L-n", lambda: layout.basic_scale(1)),
+        ("L-t", lambda: layout.move_in_stack(1)),
+        ("L-b", lambda: layout.move_in_stack(-1)),
+
+        ("L-k", lambda: layout.move(0, -1)),
+        ("L-j", lambda: layout.move(0, 1)),
+        ("L-h", lambda: layout.move(-1, 0)),
+        ("L-l", lambda: layout.move(1, 0)),
+
+        ("L-S-k", lambda: layout.move_focused_view(0, -1)),
+        ("L-S-j", lambda: layout.move_focused_view(0, 1)),
+        ("L-S-h", lambda: layout.move_focused_view(-1, 0)),
+        ("L-S-l", lambda: layout.move_focused_view(1, 0)),
+
+        ("L-C-k", lambda: layout.resize_focused_view(0, -1)),
+        ("L-C-j", lambda: layout.resize_focused_view(0, 1)),
+        ("L-C-h", lambda: layout.resize_focused_view(-1, 0)),
+        ("L-C-l", lambda: layout.resize_focused_view(1, 0)),
+
+
+
+
+        ("A-Return", lambda: os.system("sakura &")),
+        ("A-S-a", lambda: os.system("sakura &")),
+        ("A-S-d", lambda: os.system("wofi --show drun &")),
+
+
+
 
         ("XF86MonBrightnessUp", lambda: backlight_manager.set(backlight_manager.get() + 0.1)),
         ("XF86MonBrightnessDown", lambda: backlight_manager.set(backlight_manager.get() - 0.1)),
@@ -73,14 +93,15 @@ def key_bindings(layout: Layout) -> list[tuple[str, Callable[[], Any]]]:
         ("XF86AudioRaiseVolume", lambda: pactl.volume_adj(5)),
         ("XF86AudioLowerVolume", lambda: pactl.volume_adj(-5)),
         ("XF86AudioMute", lambda: pactl.mute()),
+
     ]
 
 panels = {
     'lock': {
-        'cmd': 'alacritty -e newm-panel-basic lock',
+        'cmd': 'sakura -e newm-panel-basic lock',
     },
     'launcher': {
-        'cmd': 'alacritty -e newm-panel-basic launcher'
+        'cmd': 'sakura -e newm-panel-basic launcher'
     },
     'top_bar': {
         'native': {
