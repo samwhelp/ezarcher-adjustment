049f2413bf225c98fb349c2fc46ad08a82cc5c72
samwhelp <samwhelp@users.noreply.github.com>
2023-03-23 19:58:04 +0800

    ix

--- prototype/style/qt-style-follow-gtk/config/qt-style-follow-gtk/qt-style-follow-gtk.sh
+++ prototype/style/qt-style-follow-gtk/config/qt-style-follow-gtk/qt-style-follow-gtk.sh
@@ -3,9 +3,9 @@ if [ "$XDG_CURRENT_DESKTOP" != "KDE" ]; then
 
 	export QT_QPA_PLATFORMTHEME=gtk2
 
-	export QT_QPA_PLATFORMTHEME=qt5ct
+	#export QT_QPA_PLATFORMTHEME=qt5ct
 
-	export QT_QPA_PLATFORMTHEME=kvantum
+	#export QT_QPA_PLATFORMTHEME=kvantum
 
 
 
