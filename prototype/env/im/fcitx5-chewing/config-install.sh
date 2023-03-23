b76ad0b777835dd47711def932c693e59e56e138
samwhelp <samwhelp@users.noreply.github.com>
2023-03-23 17:09:23 +0800

    fix

--- prototype/env/im/fcitx5-chewing/config-install.sh
+++ prototype/env/im/fcitx5-chewing/config-install.sh
@@ -43,27 +43,46 @@ fcitx5_config_install () {
 }
 
 fcitx5_config_install_im_config () {
+
 	#fcitx5_config_install_im_config_by_command
-	fcitx5_config_install_im_config_by_pam_environment
+	#fcitx5_config_install_im_config_by_pam_environment
+	fcitx5_config_install_im_config_by_profile_d
+
 }
 
 fcitx5_config_install_im_config_by_command () {
-	echo
 
+	echo
 	echo "im-config -n fcitx5"
 	im-config -n fcitx5
 
+	echo
 	echo "cat ~/.xinputrc"
 	cat ~/.xinputrc
 
 	echo
+
 }
 
 fcitx5_config_install_im_config_by_pam_environment () {
 
 	echo
-	echo "install -m 644 ./config/fcitx5/.pam_environment $HOME/.pam_environment"
-	install -m 644 ./config/fcitx5/.pam_environment $HOME/.pam_environment
+	echo "install -Dm644 ./config/pam/.pam_environment $HOME/.pam_environment"
+	install -Dm644 ./config/pam/.pam_environment $HOME/.pam_environment
+
+
+	echo
+
+}
+
+fcitx5_config_install_im_config_by_profile_d () {
+
+	echo
+	echo "sudo install -Dm644 ./config/profile.d/fcitx5.sh /etc/profile.d/fcitx5.sh"
+	sudo install -Dm644 ./config/profile.d/fcitx5.sh /etc/profile.d/fcitx5.sh
+
+
+	echo
 
 }
 
