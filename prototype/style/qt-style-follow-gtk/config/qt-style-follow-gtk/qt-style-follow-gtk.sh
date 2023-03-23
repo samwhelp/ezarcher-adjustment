4bf536b09c85eafea35c277da9599b3ad8778302
samwhelp <samwhelp@users.noreply.github.com>
2023-03-23 19:51:30 +0800

    fix

--- prototype/style/qt-style-follow-gtk/config/qt-style-follow-gtk/qt-style-follow-gtk.sh
+++ prototype/style/qt-style-follow-gtk/config/qt-style-follow-gtk/qt-style-follow-gtk.sh
@@ -1,4 +1,20 @@
 
 if [ "$XDG_CURRENT_DESKTOP" != "KDE" ]; then
+
 	export QT_QPA_PLATFORMTHEME=gtk2
+
+	export QT_QPA_PLATFORMTHEME=qt5ct
+
+	export QT_QPA_PLATFORMTHEME=kvantum
+
+
+
+	#export QT_STYLE_OVERRIDE=gtk2
+
 fi
+
+
+##
+## * https://wiki.archlinux.org/title/Uniform_look_for_Qt_and_GTK_applications#QGtkStyle
+## * https://wiki.archlinux.org/title/qt
+##
