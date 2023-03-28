0a497846fb0f3787ebec3f66bc3aabc6b35f0073
samwhelp <samwhelp@users.noreply.github.com>
2023-03-28 11:08:52 +0800

    fix

---
+++ prototype/look/style/profile/colloid-teal-dark/ext/config.sh
@@ -0,0 +1,6 @@
+THE_GHOME_SHELL_THEME_NAME="Colloid-Teal-Dark"
+THE_WM_THEME_NAME="Colloid-Teal-Dark"
+THE_GTK_THEME_NAME="Colloid-Teal-Dark"
+THE_ICON_THEME_NAME="Colloid-teal-dark"
+THE_CURSOR_THEME_NAME="Qogir"
+THE_CURSOR_SIZE="24"
