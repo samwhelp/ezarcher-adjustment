8808630ec372bb1b4c153fcfc0d947fb23284490
samwhelp <samwhelp@users.noreply.github.com>
2023-03-28 13:27:58 +0800

    fix

---
+++ prototype/look/style/profile/colloid-yellow-dark/ext/config.sh
@@ -0,0 +1,6 @@
+THE_GHOME_SHELL_THEME_NAME="Colloid-Yellow-Dark"
+THE_WM_THEME_NAME="Colloid-Yellow-Dark"
+THE_GTK_THEME_NAME="Colloid-Yellow-Dark"
+THE_ICON_THEME_NAME="Tela-yellow-dark"
+THE_CURSOR_THEME_NAME="Qogir"
+THE_CURSOR_SIZE="24"
