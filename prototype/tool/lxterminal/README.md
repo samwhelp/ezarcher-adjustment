e834f15fc5f619154a9661f81b052d30da9e2b76
samwhelp <samwhelp@users.noreply.github.com>
2023-04-26 16:59:41 +0800

    fix

--- prototype/tool/lxterminal/README.md
+++ prototype/tool/lxterminal/README.md
@@ -1,14 +1,20 @@
 
 # lxterminal
 
-## Package
-
-* [https://packages.ubuntu.com/jammy/lxterminal](https://packages.ubuntu.com/jammy/lxterminal)
-
 
 ## Config File Path
 
 * [~/.config/lxterminal/lxterminal.conf](config/lxterminal/lxterminal.conf)
+
+
+## Arch Package
+
+* [lxterminal](https://archlinux.org/packages/community/x86_64/lxterminal/)
+
+
+## Arch Manpage
+
+* man [lxterminal](https://man.archlinux.org/man/community/lxterminal/lxterminal.1.en)
 
 
 ## Howto
--- prototype/tool/lxterminal/config-install.sh
+++ prototype/tool/lxterminal/config-install.sh
@@ -6,33 +6,90 @@ set -e
 ################################################################################
 ### Head: lxterminal
 ##
+
 lxterminal_config_install () {
 
+	echo
+	echo "##"
 	echo "## Config: lxterminal"
+	echo "##"
 	echo
 
-	echo "mkdir -p $HOME/.config/lxterminal"
-	mkdir -p "$HOME/.config/lxterminal"
 
-	echo "install -Dm644 ./config/lxterminal/lxterminal.conf $HOME/.config/lxterminal/lxterminal.conf"
-	install -Dm644 "./config/lxterminal/lxterminal.conf" "$HOME/.config/lxterminal/lxterminal.conf"
+	lxterminal_config_install_by_dir
+
+	#lxterminal_config_install_by_each_file
+
 
 	echo
+
 }
+
+lxterminal_config_install_by_dir () {
+
+
+	echo
+	echo "mkdir -p ${HOME}/.config/lxterminal"
+	mkdir -p "${HOME}/.config/lxterminal"
+
+
+	echo
+	echo "cp -rf ./config/lxterminal/. ${HOME}/.config/lxterminal"
+	cp -rf "./config/lxterminal/." "${HOME}/.config/lxterminal"
+
+
+}
+
+lxterminal_config_install_by_each_file () {
+
+
+	echo
+	echo "mkdir -p ${HOME}/.config/lxterminal"
+	mkdir -p "${HOME}/.config/lxterminal"
+
+	echo
+	echo "install -Dm644 ./config/lxterminal/lxterminal.conf ${HOME}/.config/lxterminal/lxterminal.conf"
+	install -Dm644 "./config/lxterminal/lxterminal.conf" "${HOME}/.config/lxterminal/lxterminal.conf"
+
+
+
+}
+
 ##
 ### Tail: lxterminal
 ################################################################################
 
 
 ################################################################################
-### Head: main
+### Head: config_install
 ##
+
 main_config_install () {
+
 	lxterminal_config_install
+
 }
-## start
-main_config_install
 
 ##
-### Tail: main
+### Tail: config_install
+################################################################################
+
+
+################################################################################
+### Head: Main
+##
+
+__main__ () {
+
+	main_config_install
+
+}
+
+##
+## Start
+##
+__main__
+
+##
+### Tail: Main
 ################################################################################
